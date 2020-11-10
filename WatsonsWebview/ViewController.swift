1badb122dbeaac37b6958f3e65508ad65f84ebbd
CJ Lin <ejames.lin@gmail.com>
2020-11-10 17:44:41 +0800

    Add webview

--- WatsonsWebview/ViewController.swift
+++ WatsonsWebview/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let webview = WKWebView()
+        view.addSubview(webview)
+        webview.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            webview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            webview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        webview.load(URLRequest(url: URL(string: "https://www.watsons.com.tw")!))
     }
-
-
 }
-
